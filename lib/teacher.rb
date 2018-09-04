26f37c3bffa4774a35b52ba561ac46250ca35fa8
katy@karstaway.com <katy@karstaway.com>
2018-09-04 20:21:53 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < User
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  
   
     def initialize
     @first_name = first_name
@@ -8,6 +8,7 @@ class Teacher < User
   end
   
   def teach (knowledge)
+    KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
     "knowledge"
   end
  end
